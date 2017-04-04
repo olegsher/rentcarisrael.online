c0b614fb24e8f3de68e97f2fbde46a3209bdf293
olegsher <github@sher.biz>
2017-04-04 14:21:48 +0300

    rentcarisrael.online

--- en/index.php
+++ en/index.php
@@ -105,11 +105,6 @@ $message .= "</body></html>";
 <?php include("__header.html"); ?>
 </head>
 <body>
-<!-- <?php include("__facebook.html"); ?>
-<?php include("__google.html"); ?>
-<?php include("__mailru.html"); ?>
-<?php include("__yandex.html"); ?> -->
-// # <!-- <?php include("__shema.html"); ?> -->
 
 <?php include("__top.html"); ?>
 <?php include("_index.php"); ?>
