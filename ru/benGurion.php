e7c6c1f6088b8a54f2dc0c457b5ea1988c413401
olegsher <github@sher.biz>
2017-04-12 11:20:35 +0300

    rentcarisrael.online

---
+++ ru/benGurion.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+<title>Недорогая аренда прокат автомобиля в Израиле | RentCarIsrael.online</title>
+<meta name="keywords" content="машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
+<meta  name="description"  content="аренда прокат автомобиля в Израиле, эконом класс и семейные машины, микроавтобусы и минивэны 7-8-9 мест" />
+<meta property="fb:app_id" content="1322511981092474" />
+<meta property="og:type" content="website" />
+<meta property="og:locale" content="ru_RU" />
+<meta property="og:url" content="http://www.rentcarisrael.online" />
+<meta property="og:image" content="http://www.rentcarisrael.online/images/thankyoucar.png"/>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="640" />
+<meta property="og:image:height" content="320" />
+<meta property="og:title" content="Аренда прокат автомобиля в Израиле, эконом класс и семейные машины, микроавтобусы и минивэны" />
+<meta property="og:description" content="Лучшие цены, нет предоплаты, заказ онлайн, Whatsapp/Viber +972-58-7710101"/>
+<?php include("__header.html"); ?>
+</head>
+<body>
+<?php include("__facebook.html"); ?>
+<?php include("__google.html"); ?>
+<?php include("__mailru.html"); ?>
+<?php include("__yandex.html"); ?>
+<?php include("__shema.html"); ?>
+
+<?php include("__top.html"); ?>
+
+
+
+
+
+
+
+<?php include("__footer.html"); ?>
+
+<script type="text/javascript">
+var ZCallbackWidgetLinkId  = '86c302f105eb53a8f2c43e54f6be69ad';
+var ZCallbackWidgetDomain  = 'my.zadarma.com';
+(function(){
+    var lt = document.createElement('script');
+    lt.type ='text/javascript';
+    lt.charset = 'utf-8';
+    lt.async = true;
+    lt.src = 'https://' + ZCallbackWidgetDomain + '/callbackWidget/js/main.min.js?unq='+Math.floor(Math.random(0,1000)*1000);
+    var sc = document.getElementsByTagName('script')[0];
+    if (sc) sc.parentNode.insertBefore(lt, sc);
+    else document.documentElement.firstChild.appendChild(lt);
+})();
+</script>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+<div class="container">
+<div class="col-md-7 bann-info wow fadeInRight animated" data-wow-delay=".5s">
+<img src="images/catW.jpg" alt="машина прокат, машина прокат Израиле, аренда машины Израиль" class="img-responsive" />
+<br /><h1>ОТДЕЛЕНИЯ ПО АРЕНДЕ ПРОКАТУ АВТОМОБИЛЕЙ В ИЗРАИЛЕ:</h1>
+</div>
+<div class="clearfix"></div>
+
+
+
+Отделение проката автомобилей в аэропорту Бен Гурион является одним из ведущих отделений аренды машин в Израиле. В отделении огромный парк автомобилей практически всех категорий. 
+Здесь, в отделении проката аэропорта Бен Гурион, вы можете арендовать минивэны, автомобили класса люкс, семейные машины а также бюджетные и эконом класс машины. Обслуживание на высшем уровне, 
+работники отделения аэропорта Бен Гурион сделают все чтоб клиент остался доволен и уехал из отделения на подходящей комфортабельной машине.
+
+Автомобили напрокат аренда отделение Эйлат
+Автомобили напрокат аренда отделение Эйлат
+
+Заказ аренды онлайн
+Всем деловым и ценящим свое время людям необходим соответствующий их статусу комфортабельный транспорт, но не все компании имеют соответствующий автопарк автомобилей делового класса и класса люкс. 
+У нас в отделении по прокату аренде машин в аэропорту Бен Гурионвы всегда можете арендовать авто бизнес-класса на наиболее выгодных условиях, чтобы подчеркнуть свой высокий статус без лишних трат и хлопот.
+
+Автомобили напрокат аренда отделение Эйлат
+Автомобили напрокат аренда отделение Эйлат
+
+Заказ аренды онлайн
+Путешествуете большой семьей? Вас больше 5 человек и вы не умещаетесь в обычную машину? Тогда в нашем отделении по прокату аренде машин в аэропорту Бен Гурион вы всегда можете заказать минивэны и небольшие микроавтобусы.
+
+Автомобили напрокат аренда отделение Эйлат
+Автомобили напрокат аренда отделение Эйлат
+
+Заказ аренды онлайн
+Нуждаетесь в небольшой и недорогой машине чтобы с комфортом попутешествовать по Израилю? У нас в отделении по прокату аренде машин в аэропорту Бен Гурион вы всегда можете арендовать авто эконом-класса на наиболее выгодных условиях без лишних затрат и хлопот. 
+Подходит для молодых водителей начиная с 21 года и водительским стажем год+. На большинство категорий B (EBMR), C (ECAR), D (CCAR), E (CDAR, F (IDAR), I (SDAR), H (FDAR), M (PDAR) есть свободное бронирование
+
+Для оформления контракта арендатор должен иметь при себе зарегистрированную на его имя кредитную карту и действующие права.
+Мы принимаем кредитные карты: Visa, MasterCard, Amex, Diners
+
+
+
+
+
+
+</div>
+</div>
+
+
+
+</body>
+</html>
