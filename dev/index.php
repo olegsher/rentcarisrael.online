7f21722ccc861e679bd7e65ed3c5a8adc7deb0b9
Oleg Sher <github@sher.biz>
2017-04-29 12:38:24 +0300

    rentcarisrael.online - dev site

--- dev/index.php
+++ dev/index.php
@@ -92,7 +92,7 @@ $message .= "</body></html>";
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Car|Rental Landingpage</title>
+  <title>Car Rental Israel by Vastama Ltd</title>
 
 
   <!-- Bootstrap -->
